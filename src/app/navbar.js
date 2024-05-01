34630e817988a71e60158fcffbdbcf0d878fd834
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2024-05-01 11:15:22 +0500

    added motion

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 
 
+
 export default function Nav() {
 
     return (
@@ -10,7 +11,6 @@ export default function Nav() {
        
        <Link href="/"> <h1 className="font-bold text-2xl md:text-3xl">David Laid Workouts</h1></Link>   
         </div>
-
 
       
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Nav from "./navbar";
 import Image from "next/image";
 import HeroImage from "/public/AdobeStock_135096108.jpeg";
+import { MotionDiv } from "./Components/motiondiv";
 
 export default function Home() {
   return (
@@ -10,8 +11,17 @@ export default function Home() {
         src={HeroImage}
         sizes="(max-width: 768px), (max-width: 1200px)"
         alt="Hero picture"
-        className="pr-6 pl-6 pb-6"
+        className=""
       />
+      <div className="flex flex-col w-10 h-[20rem] items-center">
+
+        <MotionDiv
+          initial={{ height: 0 }}
+          whileInView={{ height: "100%" }}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          className=" h-full w-[3px] mt-7 rounded-md bg-gradient-to-b from-[#7ee787]"
+        ></MotionDiv>
+      </div>
     </div>
   );
 }
