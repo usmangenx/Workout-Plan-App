41d8cebc4c6a14d3ea2e98a85496539dbb9e80f4
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2024-05-16 03:02:25 +0500

    changed logo

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,5 +1,6 @@
 "use client";
-import Link from "next/link";
+import Link from "next/link"
+import Image from "next/image";
 
 
 
@@ -9,7 +10,14 @@ export default function Nav() {
     return (
       <div className="flex justify-center  items-center text-white mx-auto h-[6rem]">
        
-       <Link href="/"> <h1 className="font-bold text-3xl md:text-4xl">David Laid Workouts</h1></Link>   
+       <Link href="/"> <Image
+              src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG1mazUydm4xdjJzMnRicW9vczdpdzg0YzR6ZjExcnNlMXl1ZzRjaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qKMmvhk2QAbYo1XnmY/giphy.gif"
+              width={500}
+              height={500}
+              alt="Incline bench press"
+              unoptimized={true}
+              className="mt-3"
+            /></Link>   
         </div>
         
 
