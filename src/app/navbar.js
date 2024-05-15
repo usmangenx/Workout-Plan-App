3e54597a37d880671d7b1b5aa9b6594b422e7dcd
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2024-05-16 03:03:09 +0500

    Update navbar.js

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -8,7 +8,7 @@ import Image from "next/image";
 export default function Nav() {
 
     return (
-      <div className="flex justify-center  items-center text-white mx-auto h-[6rem]">
+      <div className="flex justify-center mr-3 ml-3 items-center text-white mx-auto h-[6rem]">
        
        <Link href="/"> <Image
               src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG1mazUydm4xdjJzMnRicW9vczdpdzg0YzR6ZjExcnNlMXl1ZzRjaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qKMmvhk2QAbYo1XnmY/giphy.gif"
