e6532849b6ba0f59176899c5e772e1422b113ab7
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2024-05-17 05:08:21 +0500

    changed logo

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link"
 import Image from "next/image";
+import Logo from "/public/Untitled (5).png"
 
 
 
@@ -11,12 +12,12 @@ export default function Nav() {
       <div className="flex justify-center mr-3 ml-3 items-center text-white mx-auto h-[6rem]">
        
        <Link href="/"> <Image
-              src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG1mazUydm4xdjJzMnRicW9vczdpdzg0YzR6ZjExcnNlMXl1ZzRjaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qKMmvhk2QAbYo1XnmY/giphy.gif"
+              src={Logo}
               width={500}
               height={500}
-              alt="Incline bench press"
-              unoptimized={true}
-              className="mt-3"
+              alt="logo"
+              className=""
+              priority={true}
             /></Link>   
         </div>
         
