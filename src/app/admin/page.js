56bf11ffd7f9705019a8f94ccd9a653606a60174
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2024-05-30 15:00:48 +0500

    Update page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,72 +1,27 @@
-"use client";
-import { useState, useEffect } from "react";
+import { notFound } from "next/navigation";
 import AdminNav from "../Components/adminnav";
-import { notFound } from "next/navigation"; 
 
 async function getData() {
-  const res = await fetch(
-    "https://davidlaidworkout.vercel.app/api/subscribers",
-    {
-      cache: "no-store",
-    }
-  );
+  const res = await fetch("https://davidlaidworkout.vercel.app/api/subscribers", {
+    cache: "no-store",
+  });
   if (!res.ok) return notFound();
   return res.json();
 }
 
-const Admin = () => {
-  const [subscribers, setSubscribers] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchSubscribers() {
-      const data = await getData();
-      setSubscribers(data);
-    }
-    fetchSubscribers();
-  }, []);
-
-  const handleRemove = async (subscriberId) => {
-    try {
-      setLoading(true); // Set loading to true when remove operation starts
-      const res = await fetch(
-        `https://davidlaidworkout.vercel.app/api/unsubscribe?subscriberId=${subscriberId}`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (!res.ok) throw new Error("Failed to remove subscriber");
-      // Remove the subscriber from the local state
-      setSubscribers((prevSubscribers) =>
-        prevSubscribers.filter((subscriber) => subscriber._id !== subscriberId)
-      );
-    } catch (error) {
-      console.error("Error removing subscriber:", error);
-    } finally {
-      setLoading(false); // Set loading to false after remove operation completes
-    }
-  };
-
+const Admin = async () => {
+  const data = await getData();
   return (
     <div className="max-w-[1300px] mx-auto min-h-screen flex flex-col">
       <AdminNav />
       <div className="flex flex-col flex-grow items-center">
-        <h1 className="text-white text-2xl mt-7">Subscribers</h1>
-        {loading && <p>Loading...</p>}
-        {!loading && subscribers.map((subscriber) => (
-          <div
-            key={subscriber._id}
-            className="text-white text-xl mt-5 w-[400px] h-[100px] p-9 rounded-lg bg-[#0f0f0f] flex justify-between items-center"
-          >
-            <div>{subscriber.email}</div>
-            <button
-              onClick={() => handleRemove(subscriber._id)}
-              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-            >
-              Remove
-            </button>
-          </div>
-        ))}
+        <h1 className="text-white text-2xl mt-7 ">Subscribers</h1>
+        {data &&
+          data.map((e) => (
+            <div key={e._id} className="text-white text-xl mt-5 w-[400px] h-[100px] p-9 rounded-lg bg-[#0f0f0f]">
+              {e.email}
+              </div>
+          ))}
       </div>
     </div>
   );
